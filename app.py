b4103daafe5bc57cc97f290ca5078215f44c880c
nanabananaxapp <nanabananaxapp@gmail.com>
2025-09-20 21:56:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -455,7 +455,7 @@ def generate_images():
         
         st.session_state.uploaded_image_urls = upload_files_to_fal(st.session_state.uploaded_file_objects)
         
-        base_prompt = "Amazing details skin-texture, top camera quality, refine details, enhanced quality!! 8k, very detailed,high-definition, high-fidelity, high-resolution, dslr quality. always keep photo resolution proportions. and make this photo as I / We are: "
+        base_prompt = "Amazing details skin-texture, top camera quality, refine details, enhanced quality!! 8k, very detailed,high-definition, high-fidelity, high-resolution, dslr quality.  "
         final_prompt = base_prompt + st.session_state.prompt
 
         arguments = {
@@ -623,4 +623,5 @@ with st.expander("⚙️ Advanced Settings"):
     seed_input = st.number_input("Seed (Optional, leave empty for random)", value=None, step=1, format="%d")
     st.session_state.seed = seed_input
     st.session_state.enable_safety_checker = st.checkbox("✅ Enable Safety Checker", value=st.session_state.enable_safety_checker)
+
 
